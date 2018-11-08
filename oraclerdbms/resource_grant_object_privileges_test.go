472c2c192aac651d2a2e577e3e0538864a42522b
Björn Ahl <bjorn.ahl@gmail.com>
2018-11-08 16:21:14 +0100

    Build version v0.2.3 of the provider (#11)
    * Updating vendoring for terraform-oracle-rdbms-helper to 0.2.2
    
    * Added first alpha support for grants on schema level. Only support for tables for a start
    
    * Building docker images for version 0.2.3

--- oraclerdbms/resource_grant_object_privileges_test.go
+++ oraclerdbms/resource_grant_object_privileges_test.go
@@ -25,6 +25,21 @@ func TestAccGrantObjPrivs(t *testing.T) {
 		},
 	})
 }
+
+func TestAccGrantObjPrivsOnSchema(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccGrantObjPrivsOnSchemaConfigBasic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("oraclerdbms_grant_object_privilege.grantobjschematest", "owner", "SYSTEM"),
+				),
+			},
+		},
+	})
+}
 func TestAccGrantObjPrivs_importBasic(t *testing.T) {
 	resourceName := "oraclerdbms_grant_object_privilege.grantobjtest"
 
@@ -65,12 +80,25 @@ resource "oraclerdbms_user" "userobjpriv" {
 resource "oraclerdbms_grant_object_privilege" "grantobjtest" {
 	grantee = "${oraclerdbms_user.userobjpriv.id}"
 	privilege = ["SELECT"]
-	owner = "SYSTEM"
+	owner = "system"
 	object = "TEST"
 
 }
 resource "oraclerdbms_user" "userobjpriv" {
 	username = "USER999"
+	password = "change_on_install"
+	default_tablespace = "USERS"
+}
+`
+	testAccGrantObjPrivsOnSchemaConfigBasic = `
+resource "oraclerdbms_grant_object_privilege" "grantobjschematest" {
+	grantee = "${oraclerdbms_user.userobjpriv2.id}"
+	privilege = ["SELECT","UPDATE"]
+	owner = "SYSTEM"
+	object_type = "TABLE"
+}
+resource "oraclerdbms_user" "userobjpriv2" {
+	username = "USER99"
 	password = "change_on_install"
 	default_tablespace = "USERS"
 }
