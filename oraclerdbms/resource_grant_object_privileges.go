3bc8ce99a696e0b3eb17f2852353c4b1f5ffa238
Björn Ahl <bnal@ba78.me>
2018-11-16 10:03:50 +0100

    Calculate hash for each privileges and migrate schema version support

--- oraclerdbms/resource_grant_object_privileges.go
+++ oraclerdbms/resource_grant_object_privileges.go
@@ -19,6 +19,11 @@ func resourceGrantObjectPrivilege() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 
+		CustomizeDiff: updateComputed,
+
+		SchemaVersion: 1,
+		MigrateState:  resourceOracleRdbmsGrantObjectPrivilegeMigrate,
+
 		Schema: map[string]*schema.Schema{
 			"grantee": &schema.Schema{
 				Type:     schema.TypeString,
@@ -69,27 +74,51 @@ func resourceGrantObjectPrivilege() *schema.Resource {
 			},
 			// This need to be changes to TypeSet. There is mutiple of privileges that need to be checked. Now only support SELECT
 			"objects_sha256": &schema.Schema{
-				Type:     schema.TypeString,
+				Type:     schema.TypeMap,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 				ForceNew: true,
 				Computed: true,
 			},
 			// This need to be changes to TypeSet. There is mutiple of privileges that need to be checked. Now only support SELECT
 			"privs_sha256": &schema.Schema{
-				Type:     schema.TypeString,
+				Type:     schema.TypeMap,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 				ForceNew: true,
 				Computed: true,
 			},
 		},
-		CustomizeDiff: updateComputed,
 	}
 }
 
 func updateComputed(d *schema.ResourceDiff, meta interface{}) error {
-	if d.Get("privs_sha256").(string) != d.Get("objects_sha256").(string) {
+	log.Println("[INFO] updateComputed")
+	if d.Get("object").(string) == "" {
+		log.Println("[INFO] updateComputed inside sha256 calc")
+		objMap := map[string]string{}
+		if v, ok := d.GetOk("objects_sha256"); ok {
+			for key, value := range v.(map[string]interface{}) {
+				objMap[key] = value.(string)
+			}
+		}
+		privsMap := map[string]string{}
+		if v, ok := d.GetOk("privs_sha256"); ok {
+			for key, value := range v.(map[string]interface{}) {
+				privsMap[key] = value.(string)
+			}
+		}
+		for k, v := range objMap {
+			if privsMap[k] != v {
+				d.SetNewComputed("objects_sha256")
+				d.SetNewComputed("privs_sha256")
+				break
+			}
+		}
+	}
+	/*if d.Get("privs_sha256").(string) != d.Get("objects_sha256").(string) {
 		d.SetNewComputed("objects_sha256")
 		d.SetNewComputed("privs_sha256")
-	}
-	log.Printf("[DEBUG] updateComputed,privs_sha256: %s, objects_sha256: %s\n", d.Get("privs_sha256").(string), d.Get("objects_sha256").(string))
+	}*/
+	//log.Printf("[DEBUG] updateComputed,privs_sha256: %s, objects_sha256: %s\n", d.Get("privs_sha256").(string), d.Get("objects_sha256").(string))
 
 	return nil
 }
@@ -141,10 +170,18 @@ func resourceOracleRdbmsCreateGrantObjectPrivilege(d *schema.ResourceData, meta 
 			return err
 		}
 		id := grantObjectPrivID(d.Get("grantee").(string), d.Get("owner").(string), d.Get("object").(string))
-		var hash string
-		hash, err = client.GrantService.GetHashSchemaPrivsToUser(resourceGrantObjectPrivilege)
-		if err != nil {
-			return err
+		//var hash map[string]string
+		hash := make(map[string]string)
+		for _, v := range privilegesList {
+			hash[v], err = client.GrantService.GetHashSchemaPrivsToUser(oraclehelper.ResourceGrantObjectPrivilege{
+				Grantee:    d.Get("grantee").(string),
+				Owner:      d.Get("owner").(string),
+				ObjectName: d.Get("object").(string),
+				Privilege:  []string{v},
+			})
+			if err != nil {
+				return err
+			}
 		}
 		d.Set("objects_sha256", hash)
 		d.Set("privs_sha256", hash)
@@ -213,6 +250,7 @@ func resourceOracleRdbmsReadGrantObjectPrivilege(d *schema.ResourceData, meta in
 		we asume that we trying to import a resource
 	*/
 	if d.Get("object").(string) != "" || (d.Get("owner").(string) == "" && d.Get("grantee").(string) == "") {
+		log.Printf("[INFO] resourceOracleRdbmsReadGrantObjectPrivilege inside object level")
 		resourceGrantObjectPrivilege := oraclehelper.ResourceGrantObjectPrivilege{
 			Grantee:    grantee,
 			Owner:      owner,
@@ -225,41 +263,51 @@ func resourceOracleRdbmsReadGrantObjectPrivilege(d *schema.ResourceData, meta in
 			return err
 		}
 		log.Printf("[INFO] ReadGrantObjectPrivilege returned: %v\n", grantedObject)
-		if !d.IsNewResource() {
-			d.Set("grantee", grantee)
-			d.Set("owner", owner)
-			d.Set("object", object)
-			d.Set("privilege", grantedObject.Privileges)
-			d.Set("objects_sha256", owner)
-			d.Set("privs_sha256", object)
+		d.Set("grantee", grantee)
+		d.Set("owner", owner)
+		d.Set("object", object)
+		d.Set("privilege", grantedObject.Privileges)
+		objHash := make(map[string]string)
+		privHash := make(map[string]string)
+		for _, v := range grantedObject.Privileges {
+			objHash[v] = owner
+			privHash[v] = object
 		}
+		d.Set("objects_sha256", objHash)
+		d.Set("privs_sha256", privHash)
 
 		return nil
 	}
 	/*
 		Doing the privilege on schema level. For now we only handle tables on schema level
 	*/
-	log.Println("[DEBUG] resourceOracleRdbmsReadGrantObjectPrivilege schema level")
+	log.Println("[INFO] resourceOracleRdbmsReadGrantObjectPrivilege schema level")
 	switch {
 	case d.Get("object_type").(string) == "TABLE":
-		log.Println("[DEBUG] resourceOracleRdbmsReadGrantObjectPrivilege schema level on table level")
-		resourceGrantObjectPrivilege := oraclehelper.ResourceGrantObjectPrivilege{
-			Grantee:   grantee,
-			Owner:     owner,
-			Privilege: privilegesList,
-		}
-		privsHash, err := client.GrantService.GetHashSchemaPrivsToUser(resourceGrantObjectPrivilege)
-		log.Printf("[DEBUG] resourceOracleRdbmsReadGrantObjectPrivilege hash: %s object: %s \n", privsHash, object)
-		if err != nil {
-			return err
-		}
-		tableHash, err := client.GrantService.GetHashSchemaAllTables(resourceGrantObjectPrivilege)
-		if err != nil {
-			return err
-		}
-		if !d.IsNewResource() {
-			d.Set("objects_sha256", tableHash)
-			d.Set("privs_sha256", privsHash)
+		log.Println("[INFO] resourceOracleRdbmsReadGrantObjectPrivilege schema level on table level")
+
+		privsHash := make(map[string]string)
+		tableHash := make(map[string]string)
+		for _, v := range privilegesList {
+			resourceGrantObjectPrivilege := oraclehelper.ResourceGrantObjectPrivilege{
+				Grantee:   grantee,
+				Owner:     owner,
+				Privilege: []string{v},
+			}
+			pHash, err := client.GrantService.GetHashSchemaPrivsToUser(resourceGrantObjectPrivilege)
+			privsHash[v] = pHash
+			log.Printf("[INFO] resourceOracleRdbmsReadGrantObjectPrivilege hash: %s object: %s \n", privsHash, object)
+			if err != nil {
+				return err
+			}
+			tableHash[v], err = client.GrantService.GetHashSchemaAllTables(resourceGrantObjectPrivilege)
+			if err != nil {
+				return err
+			}
+			if !d.IsNewResource() {
+				d.Set("objects_sha256", tableHash)
+				d.Set("privs_sha256", privsHash)
+			}
 		}
 		return nil
 	}
