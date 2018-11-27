da6112eade8faebe07e3112706c7252951aea6d3
Björn Ahl <bnal@ba78.me>
2018-11-27 22:55:25 +0100

    Fixing log messages in resourceOracleRdbmsGrantObjectPrivilegeMigrate

--- oraclerdbms/resource_grant_object_privileges_migrate.go
+++ oraclerdbms/resource_grant_object_privileges_migrate.go
@@ -11,7 +11,7 @@ func resourceOracleRdbmsGrantObjectPrivilegeMigrate(
 	v int, is *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
 	switch v {
 	case 0:
-		log.Println("[INFO] Found oraclerdbms VPC GrantObjectPrivilege v0; migrating to v1")
+		log.Println("[INFO] Found oraclerdbms GrantObjectPrivilege v0; migrating to v1")
 		return migrateGrantObjectPrivilegeV0toV1(is)
 	default:
 		return is, fmt.Errorf("Unexpected schema version: %d", v)
@@ -20,7 +20,7 @@ func resourceOracleRdbmsGrantObjectPrivilegeMigrate(
 
 func migrateGrantObjectPrivilegeV0toV1(is *terraform.InstanceState) (*terraform.InstanceState, error) {
 	if is.Empty() || is.Attributes == nil {
-		log.Println("[DEBUG] Empty VPC State; nothing to migrate.")
+		log.Println("[DEBUG] Empty State; nothing to migrate.")
 		return is, nil
 	}
 
