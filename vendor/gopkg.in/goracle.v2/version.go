19bf07ffc7d795613b2ecde897d992d0197f4b8e
Björn Ahl <bnal@ba78.me>
2019-02-14 21:18:31 +0100

    Stop using vendor as gooracle have c files that not get included in vendor dir

--- vendor/gopkg.in/goracle.v2/version.go
+++
@@ -1,8 +0,0 @@
-package goracle
-
-//go:generate bash -c "set -x; curl -L https://github.com/oracle/odpi/archive/v3.1.0.tar.gz | tar xzvf - odpi-3.1.0/{embed,include,src,CONTRIBUTING.md,LICENSE.md,README.md}"
-//go:generate rm -rf odpi
-//go:generate mv odpi-3.1.0 odpi
-
-// Version of this driver
-const Version = "v2.12.1"
